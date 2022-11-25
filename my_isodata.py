a987e76aea18073fabd6fa70fdc8cb386319126e
Steven Wang <zz90183zz@gmail.com>
2022-11-26 00:27:48 +0800

    rewrite

--- my_isodata.py
+++ my_isodata.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.spatial.distance import pdist, squareform
+from scipy.spatial.distance import cdist, squareform
 class my_isodata():
     def __init__(self, data, k0, nmin, dmin, sigma):
         self.__data = data
@@ -7,33 +7,30 @@ class my_isodata():
         self.__nmin = nmin
         self.__dmin = dmin
         self.__sigma = sigma
-    def dest(self,pdata,center_list):
-        center = []
-        for i in center_list:
-            center.append(locals()[i])
-        min_index = np.argmin(squareform(pdist(pdata, center)), axis=1)
-        for i,c in enumerate(min_index):
-            locals()[center_list[c]].append(pdata[i])
-        for i in center_list:
-            return locals()[i]
-    def id_merge(self,center_list):
-        center = []
-        for i in center_list:
-            center.append(locals()[i])
-        d = np.triu(pdist(center,center))
-        a = np.where((d~=0)&(d<self.__dmin))
-
     def fit(self):
-        fcenter = np.random.choice(self.__data,self.__k0,replace=False)
-        fmin_index = np.argmin(squareform(pdist(self.__data,fcenter)),axis=1)
-        totalc=[]
-        for i in range(self.__k0):
-            locals()['c'+str(i)]=[]
-            totalc.append('c'+str(i))
-        tem_var = []
-        for i in range(self.__k0):
-            if locals()['c'+str(i)]<self.__nmin:
-                tem_var=.append(locals()['c'+str(i)])
-                del locals()['c'+str(i)]
-
-
+        center = self.__data[np.random.choice(len(self.__data),self.__k0, replace=False), :]
+        dt = cdist(self.__data,center)
+        cofdic = dict(zip(range(len(center)),center))
+        totalc = list(range(len(center)))
+        belong = np.nanargmin(dt,axis=1)
+        for i in np.unique(belong):
+            locals()['c'+str(i)] = []
+        for i in range(len(self.__data)):
+            locals()['c'+str(belong[i])].append(self.__data[i])
+        del_c = []
+        for c in totalc:
+            if len(locals()['c'+str(c)]) < self.__nmin:
+                del_c.append(c)
+                center[c] = None
+                td = cdist(locals()['c'+str(c)], center)
+                tb = np.nanargmin(td,axis=1)
+                for w in range(len(tb)):
+                    tw = tb[w]
+                    locals()['c' + str(tw)].append(locals()['c' + str(c)][w])
+        ntotalc = list(set(totalc)-set(del_c))
+        for c in ntotalc:
+            m = np.mean(locals()['c'+str(c)],axis=0)
+            center[c] = m
+        if len(ntotalc)<self.__k0/2:
+            d = np.triu(squareform(pdist(center)))
+            wd = np.where((d==d)&(d!=0)&(d<self.__dmin))
